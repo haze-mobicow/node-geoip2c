9660225513bae109993edbe9600d3d5fff6b8b6d
haze-mobicow <w.gray@mobicow.com>
2015-04-10 00:56:59 -0400

    testing c bindings

--- src/binding.cpp
+++ src/binding.cpp
@@ -9,7 +9,10 @@ extern "C" {
 }
 
 using namespace v8;
-MMDB_s mmdb;
+MMDB_s mmdbCountry;
+MMDB_s mmdbCity;
+MMDB_s mmdbIsp;
+MMDB_s mmdbNetspeed;
 static bool loaded = false;
 
 #define TYPE_COUNTRY 1;
@@ -53,38 +56,40 @@ NAN_METHOD(loadDb)
 //    NanScope();
 
     if ((args.Length() < 1) || !args[0]->IsString())
-        MY_THROW_EXCEP("No filename specified");
-    String::Utf8Value fname(args[0]->ToString());
+        MY_THROW_EXCEP("No country filename specified");
+    String::Utf8Value countryname(args[0]->ToString());
 
     if ((args.Length() < 2) || !args[1]->IsString())
-        MY_THROW_EXCEP("No type specified");
-    String::Utf8Value ndbType(args[1]->ToString());
+        MY_THROW_EXCEP("No city filename specified");
+    String::Utf8Value cityname(args[1]->ToString());
 
-    int status = MMDB_open(*fname, MMDB_MODE_MMAP, &mmdb);
+    if ((args.Length() < 3) || !args[2]->IsString())
+        MY_THROW_EXCEP("No isp filename specified");
+    String::Utf8Value ispname(args[2]->ToString());
 
-    if (status != MMDB_SUCCESS)
+    if ((args.Length() < 4) || !args[3]->IsString())
+        MY_THROW_EXCEP("No netspeed filename specified");
+    String::Utf8Value netspeedname(args[3]->ToString());
+
+
+    // load the country db
+    int status = MMDB_open(*countryname, MMDB_MODE_MMAP, &mmdbCountry);
+    // load the city db
+    int status2 = MMDB_open(*cityname, MMDB_MODE_MMAP, &mmdbCity);
+    // load the isp db
+    int status3 = MMDB_open(*ispname, MMDB_MODE_MMAP, &mmdbIsp);
+     // load the netspeed db
+    int status4 = MMDB_open(*netspeedname, MMDB_MODE_MMAP, &mmdbNetspeed);
+
+    if (status != MMDB_SUCCESS && status2 != MMDB_SUCCESS && status3 != MMDB_SUCCESS  && status4 != MMDB_SUCCESS )
     {
-        if (status == MMDB_IO_ERROR)
-            MY_THROW_EXCEP((std::string("I/O error opeining file '")+*fname+"': "+
+        if (status == MMDB_IO_ERROR && status2 == MMDB_IO_ERROR && status3 == MMDB_IO_ERROR && status4 == MMDB_IO_ERROR)
+            MY_THROW_EXCEP((std::string("I/O error opeining file '")+*countryname+"': "+
                             nonNull(strerror(errno))).c_str())
     else
-            MY_THROW_EXCEP((std::string("Can't open database file '")+*fname+"': "+
+            MY_THROW_EXCEP((std::string("Can't open database file '")+*countryname+"': "+
                             nonNull(MMDB_strerror(status))).c_str());
     }
-        std::string dbTest = std::string(*ndbType);
-        if(dbTest =="country"){
-        dbType = 1;
-        }
-        if(dbTest =="city"){
-        dbType = 2;
-        }
-        if(dbTest =="isp"){
-        dbType = 3;
-        }
-        if(dbTest =="netspeed"){
-        dbType = 4;
-        }
-
     loaded = true;
     NanReturnUndefined();
 }
@@ -99,7 +104,7 @@ NAN_METHOD(lookupIp)
         MY_THROW_EXCEP("Empty ip address string provided");
 
     int gai_error, mmdb_error;
-    MMDB_lookup_result_s result = MMDB_lookup_string(&mmdb, *ip, &gai_error, &mmdb_error);
+    MMDB_lookup_result_s countryResult = MMDB_lookup_string(&mmdbCountry, *ip, &gai_error, &mmdb_error);
 
     if (gai_error)
         MY_THROW_EXCEP((std::string("Error from getaddrinfo for ")+*ip+
@@ -108,101 +113,107 @@ NAN_METHOD(lookupIp)
     if (mmdb_error != MMDB_SUCCESS)
         MY_THROW_EXCEP((std::string("Error looking up ip: ")+nonNull(MMDB_strerror(mmdb_error))).c_str());
 
-    if (!result.found_entry)
+    if (!countryResult.found_entry)
         NanReturnNull();
 
-    // add switch for DB type
 
-        switch (dbType) {
-            case 1:{
-                Local<Object> countryInfo = NanNew<Object>();
-                MMDB_entry_data_s ccode;
-                int status = MMDB_get_value(&result.entry, &ccode, "country", "iso_code", NULL);
-                if (status != MMDB_SUCCESS || !ccode.has_data){
-                    countryInfo->Set(NanNew("countryCode"), NanNew("NA"));
-                    countryInfo->Set(NanNew("countryName"), NanNew("NA"));
-                    NanReturnValue(countryInfo);
-                }else{
-                    if (ccode.type != MMDB_DATA_TYPE_UTF8_STRING)
-                       MY_THROW_EXCEP("Unexpected data type of result for country code");
-                MMDB_entry_data_s name;
-                status = MMDB_get_value(&result.entry, &name, "country", "names", "en", NULL);
-                if (status != MMDB_SUCCESS || !name.has_data){
-                    countryInfo->Set(NanNew("countryName"), NanNew("NA"));
-                     NanReturnValue(countryInfo);
-                    }
-                if (name.type != MMDB_DATA_TYPE_UTF8_STRING)
-                    MY_THROW_EXCEP("Unexpected data type of result for continent");
+    MMDB_lookup_result_s cityResult = MMDB_lookup_string(&mmdbCity, *ip, &gai_error, &mmdb_error);
 
-                countryInfo->Set(NanNew<String>("countryCode"), NanNew<String>(ccode.utf8_string, ccode.data_size));
-                countryInfo->Set(NanNew<String>("countryName"), NanNew<String>(name.utf8_string, name.data_size));
-                NanReturnValue(countryInfo);
-                }
-                }
-                break;
-            case 2:{
-                Local<Object> cityInfo = NanNew<Object>();
-                MMDB_entry_data_s city;
-                int status = MMDB_get_value(&result.entry, &city, "city", "names", "en", NULL);
-                if (status != MMDB_SUCCESS || !city.has_data){
-                cityInfo->Set(NanNew("city"), NanNew("NA"));
-                cityInfo->Set(NanNew("region"), NanNew("NA"));
-                NanReturnValue(cityInfo);
-                }else{
-                    if (city.type != MMDB_DATA_TYPE_UTF8_STRING)
-                       MY_THROW_EXCEP("Unexpected data type of result for country code");
-                    cityInfo->Set(NanNew<String>("city"), NanNew<String>(city.utf8_string, city.data_size));
-                }
-                MMDB_entry_data_s region;
-                status = MMDB_get_value(&result.entry, &region, "subdivisions","0","iso_code", NULL);
-                if (status != MMDB_SUCCESS || !region.has_data){
-                    cityInfo->Set(NanNew("region"), NanNew("NA"));
-                    NanReturnValue(cityInfo);
-                }else{
-                    if (region.type != MMDB_DATA_TYPE_UTF8_STRING)
-                        MY_THROW_EXCEP("Unexpected data type of result for region");
-                    cityInfo->Set(NanNew<String>("region"), NanNew<String>(region.utf8_string, region.data_size));
-                }
-                NanReturnValue(cityInfo);
-                }
-                break;
+    MMDB_lookup_result_s ispResult = MMDB_lookup_string(&mmdbIsp, *ip, &gai_error, &mmdb_error);
 
-            case 3:{
-                Local<Object> providerInfo = NanNew<Object>();
-                MMDB_entry_data_s isp;
-                int status = MMDB_get_value(&result.entry, &isp, "isp",NULL);
-                if (status != MMDB_SUCCESS || !isp.has_data){
-                    providerInfo->Set(NanNew<String>("provider"), NanNew("NA"));
-                }else{
-                    if (isp.type != MMDB_DATA_TYPE_UTF8_STRING)
-                       MY_THROW_EXCEP("Unexpected data type of result for isp");
-                    providerInfo->Set(NanNew<String>("provider"), NanNew<String>(isp.utf8_string, isp.data_size));
-                }
-                NanReturnValue(providerInfo);
-                }
-                break;
+    MMDB_lookup_result_s netspeedResult = MMDB_lookup_string(&mmdbNetspeed, *ip, &gai_error, &mmdb_error);
 
-                case 4:{
-                    Local<Object> netInfo = NanNew<Object>();
-                    MMDB_entry_data_s netspeed;
-                    int status = MMDB_get_value(&result.entry, &netspeed, "connection_type",NULL);
-                    if (status != MMDB_SUCCESS || !netspeed.has_data){
-                      netInfo->Set(NanNew<String>("netspeed"), NanNew("NA"));
-                      }else{
-                        if (netspeed.type != MMDB_DATA_TYPE_UTF8_STRING)
-                           MY_THROW_EXCEP("Unexpected data type of result for netspeed");
-                        netInfo->Set(NanNew<String>("netspeed"), NanNew<String>(netspeed.utf8_string, netspeed.data_size));
-                    }
-                    NanReturnValue(netInfo);
-                    }
-                    break;
 
-            default:{
-                MY_THROW_EXCEP("switch Error");
-                }
-                break;
-        }
-// end of switch
+Local<Object> IpData = NanNew<Object>();
+
+//country
+MMDB_entry_data_s ccode;
+int status = MMDB_get_value(&countryResult.entry, &ccode, "country", "iso_code", NULL);
+if (status != MMDB_SUCCESS || !ccode.has_data){
+    IpData->Set(NanNew("countryCode"), NanNew("NA"));
+    IpData->Set(NanNew("countryName"), NanNew("NA"));
+    IpData->Set(NanNew("city"), NanNew("NA"));
+    IpData->Set(NanNew("region"), NanNew("NA"));
+    IpData->Set(NanNew("provider"), NanNew("NA"));
+    IpData->Set(NanNew("netspeed"), NanNew("NA"));
+    IpData->Set(NanNew("countryName"), NanNew("NA"));
+    NanReturnValue(IpData);
+}else{
+    if (ccode.type != MMDB_DATA_TYPE_UTF8_STRING)
+       MY_THROW_EXCEP("Unexpected data type of result for country code");
+MMDB_entry_data_s name;
+status = MMDB_get_value(&countryResult.entry, &name, "country", "names", "en", NULL);
+if (status != MMDB_SUCCESS || !name.has_data){
+    IpData->Set(NanNew("countryName"), NanNew("NA"));
+    }
+if (name.type != MMDB_DATA_TYPE_UTF8_STRING)
+    MY_THROW_EXCEP("Unexpected data type of result for continent");
+
+
+IpData->Set(NanNew<String>("countryCode"), NanNew<String>(ccode.utf8_string, ccode.data_size));
+IpData->Set(NanNew<String>("countryName"), NanNew<String>(name.utf8_string, name.data_size));
+}
+
+if (cityResult.found_entry){
+    MMDB_entry_data_s city;
+    int status = MMDB_get_value(&cityResult.entry, &city, "city", "names", "en", NULL);
+    if (status != MMDB_SUCCESS || !city.has_data){
+    IpData->Set(NanNew("city"), NanNew("NA"));
+    IpData->Set(NanNew("region"), NanNew("NA"));
+    }else{
+        if (city.type != MMDB_DATA_TYPE_UTF8_STRING)
+           MY_THROW_EXCEP("Unexpected data type of result for country code");
+        IpData->Set(NanNew<String>("city"), NanNew<String>(city.utf8_string, city.data_size));
+    }
+    MMDB_entry_data_s region;
+    status = MMDB_get_value(&cityResult.entry, &region, "subdivisions","0","iso_code", NULL);
+    if (status != MMDB_SUCCESS || !region.has_data){
+        IpData->Set(NanNew("region"), NanNew("NA"));
+    }else{
+        if (region.type != MMDB_DATA_TYPE_UTF8_STRING)
+            MY_THROW_EXCEP("Unexpected data type of result for region");
+        IpData->Set(NanNew<String>("region"), NanNew<String>(region.utf8_string, region.data_size));
+    }
+
+}else{
+    IpData->Set(NanNew("city"), NanNew("NA"));
+    IpData->Set(NanNew("region"), NanNew("NA"));
+
+}
+
+
+if (ispResult.found_entry){
+MMDB_entry_data_s isp;
+int status = MMDB_get_value(&ispResult.entry, &isp, "isp",NULL);
+if (status != MMDB_SUCCESS || !isp.has_data){
+    IpData->Set(NanNew<String>("provider"), NanNew("NA"));
+}else{
+    if (isp.type != MMDB_DATA_TYPE_UTF8_STRING)
+       MY_THROW_EXCEP("Unexpected data type of result for isp");
+    IpData->Set(NanNew<String>("provider"), NanNew<String>(isp.utf8_string, isp.data_size));
+}
+
+}else{
+IpData->Set(NanNew("provider"), NanNew("NA"));
+}
+
+
+if (netspeedResult.found_entry){
+MMDB_entry_data_s netspeed;
+int status = MMDB_get_value(&netspeedResult.entry, &netspeed, "connection_type",NULL);
+if (status != MMDB_SUCCESS || !netspeed.has_data){
+  IpData->Set(NanNew<String>("netspeed"), NanNew("NA"));
+  IpData->Set(NanNew("netspeedError"), NanNew("NO Data"));
+  }else{
+    if (netspeed.type != MMDB_DATA_TYPE_UTF8_STRING)
+       MY_THROW_EXCEP("Unexpected data type of result for netspeed");
+    IpData->Set(NanNew<String>("netspeed"), NanNew<String>(netspeed.utf8_string, netspeed.data_size));
+}
+}else{
+IpData->Set(NanNew("netspeed"), NanNew("NA"));
+IpData->Set(NanNew("netspeedError"), NanNew("Not Found"));
+}
+NanReturnValue(IpData);
 
 }
 
@@ -210,7 +221,10 @@ NAN_METHOD(unload)
 {
     if (!loaded)
         MY_THROW_EXCEP("No database is loaded");
-    MMDB_close(&mmdb);
+    MMDB_close(&mmdbCountry);
+    MMDB_close(&mmdbCity);
+    MMDB_close(&mmdbIsp);
+    MMDB_close(&mmdbNetspeed);
     loaded = false;
     NanReturnUndefined();
 }
@@ -222,10 +236,5 @@ void init(Handle<Object> exports)
   exports->Set(NanNew<String>("unload"), NanNew<FunctionTemplate>(unload)->GetFunction());
 }
 
-void Initialize (Handle<Object> exports);
 
-void init(Handle<Object> exports) {
-  NODE_SET_METHOD(exports, "hello", Method);
-}
-
-NODE_MODULE(geoip2c, Initialize)
+NODE_MODULE(geoip2c, init)
--- test.js
+++ test.js
@@ -4,10 +4,12 @@ function resolve(ip)
     var ret = geo.lookupIp(ip);
     console.log(ip, '->', JSON.stringify(ret));
 }
+console.log('loding',process.argv[2],process.argv[3],process.argv[4],process.argv[5]);
 
-geo.load(process.argv[2],process.argv[3]);
+
+geo.load(process.argv[2],process.argv[3],process.argv[4],process.argv[5]);
 console.log("==== Looking up IPv4: ====")
-resolve("1.2.3.4");
+resolve("24.200.3.4");
 console.log("==== Looking up IPv6: ====")
 resolve("2001:1388:b47:df34:5ddf:cecb:4213:cf37");
 console.log("Done");
